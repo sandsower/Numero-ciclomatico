3c84c0d753031593ac70f7a090c23bb3e2710d12
sandsower <bensandsower@gmail.com>
2010-10-28 16:44:53 -0500

    Initial Commit

---
+++ src/Reglas/Funciones.java
@@ -0,0 +1,92 @@
+package Reglas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ *
+ * @author Sanders
+ */
+public class Funciones {
+
+    char a = '(';
+    char b = ')';
+    char d = '*';
+    char e = '+';
+    char f = '-';
+    char g = '/';
+    char h = ' ';
+    char i = '{';
+    char j = '}';
+    char k = ';';
+    char l = '[';
+    char m = ']';
+    char n = '=';
+    char o = '\"';
+    char p = '\'';
+    StringBuilder tok = new StringBuilder();
+    private ArrayList arreglo = new ArrayList();
+    private int conta = 0;
+    //char arrPat[] = new char[10];
+
+    public void misToken(char c)
+    {
+      //String separador;
+       
+       if(!this.isDelimitador(c))
+       {
+           tok.append(c);
+            conta++;
+       } else {
+           if(conta > 0)
+           {
+            this.arreglo.add(tok.toString());
+            tok = new StringBuilder();
+            conta = 0;
+           }
+       }
+
+       //String token = tok.toString();
+       //Console.WriteLine("myString2 = " + myString2);
+       //separador = Character.toString(c);
+    }
+
+    public void limpiarCodigo(String linea){
+        char c;
+        for(int i=0; i< linea.length();i++)
+        {
+            c = linea.charAt(i);
+            this.misToken(c);
+        }
+    }
+
+    public boolean isDelimitador(char c){
+        if(c != a && c != b && c != d && c != e && c != f && c != g && c != h && c!= i && c != j
+                && c != k && c != l && c != m && c!=n && c!=o && c!=p)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void imprimirPalabras(){
+        Iterator it = arreglo.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next().toString());
+        }
+    }
+
+    /**
+     * @return the arreglo
+     */
+    public ArrayList getArreglo() {
+        return arreglo;
+    }
+
+    /**
+     * @param arreglo the arreglo to set
+     */
+    public void setArreglo(ArrayList arreglo) {
+        this.arreglo = arreglo;
+    }
+}
---
+++ src/Reglas/Main.java
@@ -0,0 +1,53 @@
+package Reglas;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Raúl Valadez Romo
+ */
+public class Main {
+                
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+            Funciones funciones=new Funciones();
+            File f = new File( "//home//sands//texto.txt" );
+            System.out.println(f.length());
+            BufferedReader entrada = null;
+            try
+            {
+                entrada = new BufferedReader( new FileReader( f ) );
+                String linea;
+                char c;
+                while(entrada.ready())
+                {
+                    linea = entrada.readLine();
+                    //System.out.println(linea);
+                    funciones.limpiarCodigo(linea);
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                ArrayList palabras = funciones.getArreglo();
+                funciones.imprimirPalabras();
+            try
+            {
+                entrada.close();
+            }
+            catch(IOException e1){}
+            }
+   }
+
+
+}
