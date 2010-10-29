5a3907a147d0449680c377745ed65cbbbae6af7b
Carlos Bolanos <garrisonx@gmail.com>
2010-10-29 09:40:54 -0500

    Case de chars

--- src/Reglas/Funciones.java
+++ src/Reglas/Funciones.java
@@ -9,69 +9,94 @@ import java.util.Iterator;
  */
 public class Funciones {
 
-    char a = '(';
-    char b = ')';
-    char d = '*';
-    char e = '+';
-    char f = '-';
-    char g = '/';
-    char h = ' ';
-    char i = '{';
-    char j = '}';
-    char k = ';';
-    char l = '[';
-    char m = ']';
-    char n = '=';
-    char o = '\"';
-    char p = '\'';
     StringBuilder tok = new StringBuilder();
     private ArrayList arreglo = new ArrayList();
     private int conta = 0;
     //char arrPat[] = new char[10];
 
-    public void misToken(char c)
-    {
-      //String separador;
-       
-       if(!this.isDelimitador(c))
-       {
-           tok.append(c);
-            conta++;
-       } else {
-           if(conta > 0)
-           {
-            this.arreglo.add(tok.toString());
-            tok = new StringBuilder();
-            conta = 0;
-           }
-       }
+    public void misToken(char c) {
+        //String separador;
 
-       //String token = tok.toString();
-       //Console.WriteLine("myString2 = " + myString2);
-       //separador = Character.toString(c);
+        if (!this.isDelimitador(c)) {
+            tok.append(c);
+            conta++;
+        } else {
+            if (conta > 0) {
+                this.arreglo.add(tok.toString());
+                tok = new StringBuilder();
+                conta = 0;
+            }
+        }
+
+        //String token = tok.toString();
+        //Console.WriteLine("myString2 = " + myString2);
+        //separador = Character.toString(c);
     }
 
-    public void limpiarCodigo(String linea){
+    public void limpiarCodigo(String linea) {
         char c;
-        for(int i=0; i< linea.length();i++)
-        {
+        for (int i = 0; i < linea.length(); i++) {
             c = linea.charAt(i);
             this.misToken(c);
         }
     }
 
-    public boolean isDelimitador(char c){
-        if(c != a && c != b && c != d && c != e && c != f && c != g && c != h && c!= i && c != j
-                && c != k && c != l && c != m && c!=n && c!=o && c!=p)
-        {
-            return false;
+    public boolean isDelimitador(char c) {
+
+        switch (c) {
+            case '(':
+                return true;
+
+            case ')':
+                return true;
+
+            case '*':
+                return true;
+
+            case '+':
+                return true;
+
+            case '-':
+                return true;
+
+            case '/':
+                return true;
+
+            case ' ':
+                return true;
+
+            case '{':
+                return true;
+
+            case '}':
+                return true;
+
+            case ';':
+                return true;
+
+            case '[':
+                return true;
+
+            case ']':
+                return true;
+
+            case '=':
+                return true;
+
+            case '\"':
+                return true;
+
+            case '\'':
+                return true;
+
+            default:
+                return false;
         }
-        return true;
     }
 
-    public void imprimirPalabras(){
+    public void imprimirPalabras() {
         Iterator it = arreglo.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             System.out.println(it.next().toString());
         }
     }
